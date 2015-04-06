fd32c72dafa23e1aea9dce3fa685c55a380f8e28
CATHERINE REYTO <CATHERINE.REYTO@GMAIL.COM>
2015-04-06 15:05:37 -0400

    Initial Commit

---
+++ BitmakerLabsDay1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BitmakerLabsDay1
+//
+//  Created by Cat Reyto on 2015-04-06.
+//  Copyright (c) 2015 Cat Reyto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BitmakerLabsDay1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BitmakerLabsDay1
+//
+//  Created by Cat Reyto on 2015-04-06.
+//  Copyright (c) 2015 Cat Reyto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
